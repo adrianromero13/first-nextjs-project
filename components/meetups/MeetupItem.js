0a596165ed21d7edb130c3a94b90f458ece3dd2e
Adrian Romero <radrian031613@gmail.com>
2021-09-02 08:55:00 -0700

    generated data using Next.js getStaticProps built in function and prepared components to pass down props and render data

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -1,20 +1,26 @@
 // import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Card from'../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter();
+  // button function
+  function showDetailsHandler() {
+    router.push('/' + props.id); // same as the Link element
+  }
   return (
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img href={props.image} src={props.src} alt={props.title} />
+          <img href={props.image} src={props.image} alt={props.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
