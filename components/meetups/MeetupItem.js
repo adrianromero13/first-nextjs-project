8039d5ff1663495e71e79a19af3b4d6608a9c170
Adrian Romero <radrian031613@gmail.com>
2021-09-02 17:25:39 -0700

    connected the application to a mongo database and rendered data dynamically. Form page connected to database using POST method and data rendered using the GET method

--- components/meetups/MeetupItem.js
+++ components/meetups/MeetupItem.js
@@ -13,11 +13,11 @@ function MeetupItem(props) {
     <li className={classes.item}>
       <Card>
         <div className={classes.image}>
-          <img href={props.image} src={props.image} alt={props.title} />
+          <img href={props?.image} src={props?.image} alt={props?.title} />
         </div>
         <div className={classes.content}>
           <h3>{props.title}</h3>
-          <address>{props.address}</address>
+          <address>{props?.address}</address>
         </div>
         <div className={classes.actions}>
           <button onClick={showDetailsHandler}>Show Details</button>
